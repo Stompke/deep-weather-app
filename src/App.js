4f6f3b4531dd462beb9cd1a7cb34763f50bca906
Shawn Tompke <shawntompke@gmail.com>
2020-09-11 13:37:12 -0700

    state is storing top 15 cities and alphabetizing them

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  console.log(process.env.REACT_APP_WEATHERSTACK_API_KEY)
+  const largestCities = [
+    {name: "New York", temp: null},
+    {name: "Los Angeles", temp: null},
+    {name: "Chicago", temp: null},
+    {name: "Houston", temp: null},
+    {name: "Phoenix", temp: null},
+    {name: "Philadelphia", temp: null},
+    {name: "San Antonio", temp: null},
+    {name: "San Diego", temp: null},
+    {name: "Dallas", temp: null},
+    {name: "San Jose", temp: null},
+    {name: "Austin", temp: null},
+    {name: "Jacksonville", temp: null},
+    {name: "Fort Worth", temp: null},
+    {name: "Columbus", temp: null},
+    {name: "Charlotte", temp: null}
+  ]
+  largestCities.sort(function(a, b) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    
+    if(nameA < nameB) {
+      return -1
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0 
+
+    return a.name - b.name;
+  });
+
+  const [topCities, setTopCities] = useState(largestCities)
+
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_WEATHERSTACK_API_KEY
+    axios
+    .get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=New York`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch( err => {
+      console.log(err)
+    })
+  },[])
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React {process.env.NODE_ENV}
-        </a>
+        <h2>Top Cities</h2>
+        {topCities.map(item => <div>{item.name}</div>)}
       </header>
     </div>
   );
