e446dc828bbc77940cfbfba7e32e40360081da22
Shawn Tompke <shawntompke@gmail.com>
2020-09-13 06:36:02 -0700

    added CityPage, now adding react router

---
+++ src/components/CityPage/CItyPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { container } from './CityPageStyles';
+
+const CityPage = (props) => {
+
+    return (
+        <>  
+        <h1>City Page {props.title}</h1>
+        </>
+    )
+}
+
+export default CityPage;
